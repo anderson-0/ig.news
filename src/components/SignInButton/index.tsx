7fd8c5c2de50e51590b86bb6ee2cc464844edcea
Anderson <anderson.consultant.it@outlook.com>
2021-09-29 20:44:36 -0400

    refactor: imports of local files should come after package imports

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,7 @@
-import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
 
 export function SignInButton() {
     const isUserLoggedIn = true;

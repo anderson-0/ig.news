aab8907448741f8b1636676a673fcd0f23cb93f0
Anderson <anderson.consultant.it@outlook.com>
2021-09-29 20:44:53 -0400

    feature: home page content and styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title> Início | IG.News</title>
       </Head>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome!</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>For $9.90/month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding"/>
+      </main>
     </>
   )
 }

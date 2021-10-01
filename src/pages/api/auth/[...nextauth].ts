96534ac778515020bb6d20c339ec0eb9f6afdb10
Anderson <anderson.consultant.it@outlook.com>
2021-10-01 11:01:17 -0400

    feature: included jwt signing key in next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,9 @@ export default NextAuth({
       scope: "read:user"
     }),
   ],
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_KEY
+  }
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;

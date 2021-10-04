e093fa1deba13c223758755533c5ec21625c0ce7
Anderson <anderson.consultant.it@outlook.com>
2021-10-04 01:25:50 -0400

    feature: manageSubscriptions lib for stripe checkout.session.completed hook

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,8 @@
+export async function saveSubscription(
+    subscriptionId: string, 
+    customerId: string
+) {
+    // Find the user in FaunaDB using customerId (stripe customer ID)
+
+    // Save user subscription data in FaunaDB
+}
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -41,7 +42,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const { type } = event;
         
         if(relevantEvents.has(type)) {
-            console.log(`Evento Recebido: ${type}`);
+            try {
+                switch (type) {
+                case 'checkout.session.completed':
+                    const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+                    await saveSubscription(
+                        checkoutSession.subscription.toString(),
+                        checkoutSession.customer.toString()
+                    )
+                default:
+                    throw new Error('Unhandled event error');
+            }
+            } catch (error) {
+                console.log(error);
+                return res.json({error: 'Webhook ErrorHandler failed'});
+            }
         }
 
         return res.json({ok:true})

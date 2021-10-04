1b8a6b86051c658d12f75d406fe12d9b3c1e7360
Anderson <anderson.consultant.it@outlook.com>
2021-10-04 16:26:02 -0400

    feature: change subscription status depending on action

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -4,7 +4,8 @@ import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
     subscriptionId: string, 
-    customerId: string
+    customerId: string,
+    createAction = false
 ) {
     // Find the user in FaunaDB using customerId (stripe customer ID)
     const userRef = await fauna.query(
@@ -27,10 +28,28 @@ export async function saveSubscription(
         price_id: subscription.items.data[0].price.id
     }
     // Save user subscription data in FaunaDB
-    await fauna.query(
-        q.Create(
-            q.Collection('subscriptions'),
-            { data: subscriptionData }
+    if (createAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                { data: subscriptionData }
+            )
         )
-    )
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscription.id
+                        )
+                    )
+                ),
+                { data: subscriptionData }
+            )
+        )
+    }
+    
 }

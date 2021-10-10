19085c9854cf9f098192b7635384e9d43e489988
Anderson <anderson.consultant.it@outlook.com>
2021-10-10 18:34:38 -0400

    feature: configured prismic service

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,9 +1,13 @@
 import Prismic from '@prismicio/client';
 
-export function getPrismicClient() {
-    const prismic = Prismic.client(process.env.PRISMIC_ACCESS_TOKEN,{
-        accessToken: process.env.PRISMIC_ACCESS_TOKEN
-    });
+export function getPrismicClient(req?: unknown) {
+    const prismic = Prismic.client(
+        process.env.PRISMIC_ENDPOINT,
+        {
+            accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+            req
+        }
+    );
 
     return prismic;
 }
